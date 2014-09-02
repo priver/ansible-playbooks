21830fb41a58d2ed8d8d434d19a2d63b0e9413d5
Mikhail Priver <m.priver@gmail.com>
2014-09-03 02:04:32 +0400

    Fix docstrings in Jinja2 filters

--- filter_plugins/common_filters.py
+++ filter_plugins/common_filters.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+"""Common Jinja2 filters for manipulating ansible vars."""
+
 import itertools
 import math
 import operator
@@ -6,46 +8,49 @@ import os.path
 
 
 def hostname(fqdn):
-    """Returns hostname part of FQDN."""
+    """Return hostname part of FQDN."""
     return fqdn.partition('.')[0]
 
 
 def domain(fqdn):
-    """Returns domain part of FQDN."""
+    """Return domain part of FQDN."""
     return fqdn.partition('.')[2]
 
 
 def split_filename(filename):
-    """Returns extension of filename."""
+    """Return extension of filename."""
     return os.path.splitext(filename)[0]
 
 
 def split_extension(filename):
-    """Returns filename without extension."""
+    """Return filename without extension."""
     return os.path.splitext(filename)[1]
 
 
 def rstrip_substring(name, substring):
-    """Strips given substring from the end of name."""
+    """Strip given substring from the end of name."""
     if name.endswith(substring):
         return name[:-len(substring)]
     return name
-    
+
 
 def attrs(dict_list, key):
-    """Iterates values of specified key in list of dicts."""
+    """Iterate values of specified key in list of dicts."""
     return itertools.imap(operator.itemgetter(key), dict_list)
 
 
 def ceil2(number, min_value=0):
-    """Rounds up number to the next highest power of 2."""
+    """Round up number to the next highest power of 2."""
     ceiled = 2 ** int(math.ceil(math.log(number, 2))) if number > 0 else 0
     return max(ceiled, min_value)
 
 
 class FilterModule(object):
-    """Jinja2 filters for manipulating ansible vars."""
+
+    """Common Jinja2 filters for manipulating ansible vars."""
+
     def filters(self):
+        """Return filter functions."""
         return {
             'hostname': hostname,
             'domain': domain,
--- filter_plugins/postgresql_filters.py
+++ filter_plugins/postgresql_filters.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-MULTIPLES =(
+"""Jinja2 filters for Postgresql config files."""
+MULTIPLES = (
     ('GB', 1024 ** 3),
     ('MB', 1024 ** 2),
     ('kB', 1024 ** 1),
@@ -8,6 +9,7 @@ MULTIPLES =(
 
 
 def pg_size(size):
+    """Return string representation of memory size."""
     for suffix, factor in MULTIPLES:
         if size >= factor * 10:
             break
@@ -16,39 +18,49 @@ def pg_size(size):
 
 
 def shared_buffers(mem):
+    """Calculate shared_buffers size in bytes."""
     return mem // 4
 
 
 def pg_shared_buffers(mem):
+    """Calculate shared_buffers size."""
     return pg_size(shared_buffers(mem))
 
 
 def pg_work_mem(mem, max_connections=200):
+    """Calculate work_mem size."""
     return pg_size((mem - shared_buffers(mem)) // (max_connections * 3))
 
 
 def pg_maintenance_work_mem(mem):
-    return pg_size(min(mem // 16, 2 * 1024**3))
+    """Calculate maintenance_work_mem size."""
+    return pg_size(min(mem // 16, 2 * 1024 ** 3))
 
 
 def pg_effective_cache_size(mem):
+    """Calculate effective_cache_size."""
     return pg_size((mem * 3) // 4)
 
 
 def pg_wal_buffers(mem):
-    wal_buffers = min((shared_buffers(mem) * 3) // 100, 16 * 1024**2)
-    if wal_buffers > 14 * 1024**2:
-        wal_buffers = 16 * 1024**2
+    """Calculate wal_buffers size."""
+    wal_buffers = min((shared_buffers(mem) * 3) // 100, 16 * 1024 ** 2)
+    if wal_buffers > 14 * 1024 ** 2:
+        wal_buffers = 16 * 1024 ** 2
     return pg_size(wal_buffers)
 
 
 class FilterModule(object):
+
+    """Jinja2 filters for Postgresql config files."""
+
     def filters(self):
+        """Return filter functions."""
         return {
             'pg_size': pg_size,
             'pg_shared_buffers': pg_shared_buffers,
             'pg_work_mem': pg_work_mem,
             'pg_maintenance_work_mem': pg_maintenance_work_mem,
-            'pg_effective_cache_size': pg_effective_cache_size,            
+            'pg_effective_cache_size': pg_effective_cache_size,
             'pg_wal_buffers': pg_wal_buffers,
         }
